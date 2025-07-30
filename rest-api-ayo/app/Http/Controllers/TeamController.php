7784d05a0d3eb72a349ff69260d2b6d32e0dc507
Adityo <50315153+adityoarr@users.noreply.github.com>
2025-07-30 08:20:21 +0700

    team api

--- rest-api-ayo/app/Http/Controllers/TeamController.php
+++ rest-api-ayo/app/Http/Controllers/TeamController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Team;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class TeamController extends Controller
 {
@@ -11,7 +15,8 @@ class TeamController extends Controller
      */
     public function index()
     {
-        return response()->json(['message' => 'team index'], 201);
+        $team = Team::all();
+        return response()->json($team, 201);
     }
 
     /**
@@ -19,30 +24,118 @@ class TeamController extends Controller
      */
     public function store(Request $request)
     {
-        return response()->json(['message' => 'team store'], 201);
+        try {
+            $validator = Validator::make($request->all(), [
+                'nama' => 'required',
+                'logo' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+                'tahun_berdiri' => 'required',
+                'alamat_markas' => 'required',
+                'kota_markas' => 'required'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 422);
+            }
+
+            $team = new Team();
+            $team->nama = $request->nama;
+            if ($request->hasFile('logo')) {
+                $path = $request->file('logo')->store('public/logo');
+                $url = Storage::url($path);
+                $team->logo = $url;
+            }
+            $team->tahun_berdiri = $request->tahun_berdiri;
+            $team->alamat_markas = $request->alamat_markas;
+            $team->kota_markas = $request->kota_markas;
+            $team->is_deleted = 0;
+            $team->save();
+
+            return response()->json(['message' => 'team stored'], 201);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'team not found'], 404);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        return response()->json(['message' => 'team show'], 201);
+        try {
+            $team = Team::find($id);
+            if (!empty($team)) {
+                return response()->json($team);
+            } else {
+                return response()->json(['message' => 'team not found'], 404);
+            }
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'team not found'], 404);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        return response()->json(['message' => 'team update'], 201);
+        try {
+            if (Team::where('team_id', $id)->exists()) {
+                $team = Team::find($id);
+                $team->name = is_null($request->nama) ? $team->nama : $request->nama;
+                if ($request->hasFile('logo')) {
+                    $validator = Validator::make($request->all(), [
+                        'logo' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+                    ]);
+
+                    if ($validator->fails()) {
+                        return response()->json(['errors' => $validator->errors()], 422);
+                    }
+                    $path = $request->file('logo')->store('public/logo');
+                    $url = Storage::url($path);
+                    $team->logo = $url;
+                }
+                $team->tahun_berdiri = is_null($request->tahun_berdiri) ? $team->tahun_berdiri : $request->tahun_berdiri;
+                $team->alamat_markas = is_null($request->alamat_markas) ? $team->alamat_markas : $request->alamat_markas;
+                $team->kota_markas = is_null($request->kota_markas) ? $team->kota_markas : $request->kota_markas;
+                $team->save();
+                return response()->json(['message' => 'team Updated'], 201);
+            } else {
+                return response()->json(['message' => 'team not found'], 404);
+            }
+            $team->save();
+
+            return response()->json(['message' => 'team updated'], 201);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'team not found'], 404);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        return response()->json(['message' => 'team destroy'], 201);
+        try {
+            if (Team::where('team_id', $id)->exists()) {
+                $team = Team::find($id);
+                $team->is_deleted = 1;
+                $team->save();
+                return response()->json([
+                    'message' => 'team deleted'
+                ], 202);
+            } else {
+                return response()->json(['message' => 'team not found'], 404);
+            }
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'team not found'], 404);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
     }
 }
--- rest-api-ayo/app/Models/Team.php
+++ rest-api-ayo/app/Models/Team.php
@@ -3,9 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Team extends Model
 {
     protected $table = 'teams';
     protected $primaryKey = 'team_id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = Str::uuid();
+        });
+    }
 }
